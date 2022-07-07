f8925237ead91fc780918606278c0b9768690c96
wateryinhare62 <water.yin.hare.62@gmail.com>
2022-07-07 11:24:46 +0900

    Series #13 initial upload

---
+++ modules2/src/bin/graphics.rs
@@ -0,0 +1,11 @@
+pub mod calc {
+    pub fn get_x() {}
+    pub fn get_y() {}
+}
+
+pub mod draw {
+    pub fn point() {}
+    pub fn line() {}
+    pub fn triangle() {}
+    pub fn square() {}
+}
---
+++ modules2/src/bin/use_module.rs
@@ -0,0 +1,10 @@
+mod graphics;
+
+use crate::graphics::draw;
+
+fn main() {
+    draw::point();
+    draw::line();
+    draw::triangle();
+    draw::square();
+}
